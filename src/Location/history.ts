57c6119de505078546b4b74b4b6a6eecef74571f
Lunuy <luminuy@gmail.com>
2020-03-10 13:25:10 +0900

    History

--- src/Location/history.ts
+++ src/Location/history.ts
@@ -1,13 +1,13 @@
 import location from "./location";
-
+import changyState from "../State/state";
 
 export default {
     pushState(state : any, title : string, url? : string) {
-        history.pushState(state, title, url);
+        if(!changyState.ssrDocument) history.pushState(state, title, url);
         location.pathname.set(url);
     },
     replaceState(state : any, title : string, url? : string) {
-        history.replaceState(state, title, url);
+        if(!changyState.ssrDocument) history.replaceState(state, title, url);
         location.pathname.set(url);
     }
 };
